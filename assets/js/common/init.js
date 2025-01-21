bd4ed8b54986f2e854bc0ff5c43c51ef255eec10
kuznetsov17 <m@viku.me>
2025-01-21 18:52:51 +0200

    added search

--- assets/js/common/init.js
+++ assets/js/common/init.js
@@ -1,8 +1,5 @@
+// let firstRun;
 document.addEventListener("DOMContentLoaded", function(event){
-    // enableCopy();
-    if (document.head.dataset['buildSearchIndex'] == "true") {
-      initSearch();
-    }
     setStartTheme();
     enableCopy();
   });
